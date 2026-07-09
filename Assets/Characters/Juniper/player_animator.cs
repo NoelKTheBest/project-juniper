80da35ee54ef45112b69c4f67284169a5a438ce7
Noel K. <72371650+NoelKTheBest@users.noreply.github.com>
2026-07-08 23:50:59 -0700

    Animator test

--- Assets/Characters/Juniper/player_animator.cs
+++ Assets/Characters/Juniper/player_animator.cs
@@ -1,49 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using UnityEngine;
 
-public class player_idle_states : StateMachineBehaviour
+public class player_animator : StateMachineBehaviour
 {
     // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       Debug.Log("Hola mundo");
+       Debug.Log("This is OnStateEnter");
     }
 
     // OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       Debug.Log("This is OnStateUpdate");
+    }
 
     // OnStateExit is called before OnStateExit is called on any state inside this state machine
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       Debug.Log("This is OnStateExit");
+    }
 
     // OnStateMove is called before OnStateMove is called on any state inside this state machine
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       Debug.Log("This is OnStateMove");
+    }
 
     // OnStateIK is called before OnStateIK is called on any state inside this state machine
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       Debug.Log("This is OnStateIK");
+    }
 
     // OnStateMachineEnter is called when entering a state machine via its Entry Node
     override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
     {
-       Debug.Log("Hello World");
+       Debug.Log("This is OnStateMachineEnter");
     }
 
     // OnStateMachineExit is called when exiting a state machine via its Exit Node
     override public void OnStateMachineExit(Animator animator, int stateMachinePathHash)
     {
-       Debug.Log("Goodbye World");
+       Debug.Log("This is OnStateMachineExit");
     }
 }
